823362f6758e12860d202589e98209c21b8cc938
Iusif797 <iusif797@gmail.com>
2025-05-01 22:26:25 +0200

    SEO оптимизация: мета-теги, структурированные данные, оптимизация изображений

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'styled': ['styled-components']
+        }
+      }
+    }
+  }
 })
